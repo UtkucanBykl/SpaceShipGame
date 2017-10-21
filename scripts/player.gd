323df8edbe1e590f2f1aec3cd82b1805a9159292
utkucanbykl <utkucanbykl@gmail.com>
2017-10-22 00:21:50 +0300

    Add Attack

--- scripts/player.gd
+++ scripts/player.gd
@@ -23,8 +23,11 @@ func _input(event):
 			
 		elif event.scancode == KEY_SPACE:
 			var fire = fire_scene.instance()
-			fire.set_pos(Vector2(curPos.x-350, curPos.y-250))
+			var fire2 = fire_scene.instance()
+			fire.set_pos(Vector2(curPos.x-350, curPos.y-260))
+			fire2.set_pos(Vector2(curPos.x-450, curPos.y-260))
 			get_node("/root/World").add_child(fire)
 			fire.add_to_group("fire")
-			
+			get_node("/root/World").add_child(fire2)
+			fire2.add_to_group("fire")	
 
