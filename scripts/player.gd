9686b006738a3c25192b6d17e92b8623bde94be7
utkucanbykl <utkucanbykl@gmail.com>
2017-10-26 23:26:05 +0300

    Future

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,24 +14,22 @@ func _ready():
 func _process(delta):
 	get_node("/root/World/ammo").set_text("Ammo :" + str(ammo))
 	var curPos = self.get_pos()
-	if Input.is_action_pressed("ui_right"):
+	if curPos.x < 1000 and Input.is_action_pressed("ui_right"):
 		curPos.x += addX
 		self.set_pos(curPos)
-	if Input.is_action_pressed("ui_left"):
+	if curPos.x > 0 and Input.is_action_pressed("ui_left"):
 		curPos.x -= addX
 		self.set_pos(curPos)
-	if Input.is_action_pressed("ui_select"):
+	if ammo > 0 and Input.is_action_pressed("ui_select"):
 		var fire = fire_scene.instance()
 		var fire2 = fire_scene.instance()
-		print(ammo)
-		if ammo > 0:
-			fire.set_pos(Vector2(curPos.x-350, curPos.y-260))
-			fire2.set_pos(Vector2(curPos.x-450, curPos.y-260))
-			get_node("/root/World").add_child(fire)
-			fire.add_to_group("fire")
-			get_node("/root/World").add_child(fire2)
-			fire2.add_to_group("fire")	
-			ammo -= 1
+		fire.set_pos(Vector2(curPos.x-350, curPos.y-260))
+		fire2.set_pos(Vector2(curPos.x-450, curPos.y-260))
+		get_node("/root/World").add_child(fire)
+		fire.add_to_group("fire")
+		get_node("/root/World").add_child(fire2)
+		fire2.add_to_group("fire")	
+		ammo -= 1
 			
 
 
